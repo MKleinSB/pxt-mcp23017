f46410d8298f4c2fab659d953e373c35842dc0d7
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2021-07-09 16:40:36 +0200

    first release

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
-// Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+/**
+ * It´s not necessary to define outputs. 
+ * Outputs are standard.
+ * Only inputs need to be defined.
+ */
+MCP23017.initMCP23017LED(Ports.GPA7)
+MCP23017.initMCP23017Button(Ports.GPB7)
+basic.forever(function () {
+    MCP23017.setLed(Ports.GPB0, true)
+    basic.pause(500)
+    MCP23017.setLed(Ports.GPB0, false)
+    basic.pause(500)
+})
+basic.forever(function () {
+    if (MCP23017.MCPButtonPressed(Ports.GPB7)) {
+        led.plot(2, 2)
+    } else {
+        led.unplot(2, 2)
+    }
+})
